08b59c40d1941abaf928ded8f32614152f7ca82e
Hrayr Harutyunyan <harhro@gmail.com>
2021-03-22 19:24:36 -0700

    Add DANN

---
+++ examples/algorithms/dann.py
@@ -0,0 +1,66 @@
+import torch.autograd
+
+from algorithms.single_model_algorithm import SingleModelAlgorithm
+from models.initializer import initialize_model
+from modeels.domain_classifiers import initialize_domain_classifier
+
+
+class ReverseGradLayer(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+        return output, None
+
+
+class DANN(SingleModelAlgorithm):
+    """ Unsupervised Domain Adaptation by Backpropagation.
+
+       Original paper:
+           @inproceedings{ganin2015unsupervised,
+             title={Unsupervised domain adaptation by backpropagation},
+             author={Ganin, Yaroslav and Lempitsky, Victor},
+             booktitle={International conference on machine learning},
+             pages={1180--1189},
+             year={2015},
+             organization={PMLR}
+           }
+   """
+    def __init__(self, config, d_out, grouper, loss,
+            metric, n_train_steps):
+        model = initialize_model(config, d_out).to(config.device)
+
+        self.num_domains = grouper.cardinality.item()
+        self.alpha = config.dann_alpha
+        self.beta = config.dann_beta
+        self.dc_name = config.dann_dc_name
+        self.d_out = d_out
+
+        # initialize domain classifier
+        model.domain_classifier = initialize_domain_classifier(name=self.dc_name, num_domains=self.num_domains)
+
+        # initialize module
+        super().__init__(
+            config=config,
+            model=model,
+            grouper=grouper,
+            loss=loss,
+            metric=metric,
+            n_train_steps=n_train_steps,
+        )
+
+    def process_batch(self, batch):
+        ret = super(self, DANN).process_batch(batch)
+        features = ret['features']
+        features = ReverseGradLayer(features, self.alpha)
+        ret['domain_pred'] = self.model.domain_classifier(features)
+        return ret
+
+    def objective(self, results):
+        y_loss = self.loss.compute(results['y_pred'], results['y_true'], return_dict=False)
+        domain_loss = self.loss.compute(results['domain_pred'], results['g'], return_dict=False)
+        return y_loss + self.beta * domain_loss
--- examples/configs/algorithm.py
+++ examples/configs/algorithm.py
@@ -40,5 +40,13 @@ algorithm_defaults = {
         'eval_loader': 'standard',
         'irm_lambda': 100.,
         'irm_penalty_anneal_iters': 500,
-    }
+    },
+    'DANN': {
+        'train_loader': 'standard',
+        'uniform_over_groups': False,
+        'eval_loader': 'standard',
+        'dann_beta': 1.0,
+        'dann_alpha': 1.0,
+        'dann_dc_name': 'unspecified'
+    },
 }
